ee05f60b565e116d56f0b3e137d85b7f5c90db2c
Daniel <dlw1ll27@gmail.com>
2021-06-22 14:06:19 -0700

    fixes csv parsing issue. fully working app.js api

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var fs = require('fs');
 var csv = require('csv-parser');
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 
+let abbrevs = [];
+fs.createReadStream('abbreviations.csv')
+  .pipe(csv())
+  .on('data', (data) => {
+    abbrevs.push(data)
+    console.log("abbrevs data[0]:===============================================", data)
+  })
+  .on('end', () => {
+    console.log("abbrevs:===================================================", abbrevs);
+  });
 let state_data = [];
 fs.createReadStream('state_data.csv')
   .pipe(csv())
@@ -12,13 +21,10 @@ fs.createReadStream('state_data.csv')
     console.log("state_data:===================================================", state_data[0]["state"]);
   });
 
-let abbrevs = [];
-fs.createReadStream('abbreviations.csv')
-  .pipe(csv())
-  .on('data', (data) => abbrevs.push(data))
-  .on('end', () => {
-    console.log("abbrevs:===================================================", abbrevs[0]["Code"]);
-  });
+
+  //considering that you said I did well, is there any possibilty for an internship? Wanna get my foot in the door and gain experience and i feel paynearme
+  //would be a great place to get that experience.If not do you know if you will have internship or 
+  //apprenticeship roles later? I'd like to keep in touch for if/when those roles  open
 
   //This will return all the state/abbreviations
 app.get("/states", function(req, res) {
@@ -44,7 +50,22 @@ app.listen(3000, function(){
 
 //this will parse params and return data for the specified state
 app.get('/states/:abbreviation', function(req, res){
-  res.send(req.params.abbreviation);
+  //we need to associate an AABREVIATION with the correct STATE DATA
+  let state = "";
+  let temp_abbrev = "";
+  let index = 0;
+  while(index < abbrevs.length){
+    if (abbrevs[index]["Code"] == req.params.abbreviation){
+      console.log(abbrevs[index]["State"]);
+      temp_abbrev = abbrevs[index]["State"];
+      state = `${abbrevs[index]["State"]}, ${req.params.abbreviation} `
+      break;
+    } else {
+      index += 1;
+    }
+  }
+  console.log(state);  
+  res.send(state);
 });
 
 
