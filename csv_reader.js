8b93ee2fc6b98a5f467be454916677743beaf1a6
Daniel Williams <dlw1ll27@gmail.com>
2021-06-30 20:12:27 -0700

    updates to server

--- csv_reader.js
+++ csv_reader.js
@@ -1,29 +1,41 @@
+/* eslint-disable camelcase */
 var fs = require('fs');
 var csv = require('csv-parser');
 
+function get_abbrevs() {
 //get all abbreviations
-let abbrevs = [];
-fs.createReadStream('abbreviations.csv')
-  .pipe(csv())
-  .on('data', (data) => {
-    abbrevs.push(data)
-    // console.log("abbrevs data[0]:===============================================", data)
-  })
-  .on('end', () => {
+  let abbrevs = [];
+  fs.createReadStream('abbreviations.csv')
+    .pipe(csv())
+    .on('data', (data) => {
+      abbrevs.push(data);
+    // console.log("abbrevs data[0]:===============================================", data);
+    })
+    .on('end', () => {
     // console.log("abbrevs:===================================================", abbrevs);
-  });
+    });
+  return abbrevs;
+}
 
-  //get all state data
-let state_data = [];
-fs.createReadStream('state_data.csv')
-  .pipe(csv())
-  .on('data', (data) => state_data.push(data))
-  .on('end', () => {
-    // console.log("state_data:===================================================", state_data[0]["state"]);
-  });
+function get_states() {
+//get all state data
+  let states_data = [];
+  fs.createReadStream('state_data.csv')
+    .pipe(csv())
+    .on('data', (data) => {
+      states_data.push(data);
+      console.log("getting state data:===============================================", data);
 
+    })
+    .on('end', () => {
+      console.log("FINAL state data:===================================================", states_data[0]["state"]);
+    });
+ 
+  console.log("helooooooooooo");
+
+  return states_data;
+
+}
 // get state/abbrev data
-
-
-  module.exports = {state_data};
-  module.exports = {abbrevs};
+module.exports = {get_states};
+module.exports = {get_abbrevs};
--- server.js
+++ server.js
@@ -1,30 +1,31 @@
+/* eslint-disable camelcase */
 var fs = require('fs');
 var csv = require('csv-parser');
-// const {state_data} = require('./csv_reader.js');
-// const {abbrevs} = require('./csv_reader.js');
+const {get_abbrevs} = require('./csv_reader.js');
+const {get_states} = require('./csv_reader.js');
 const path = require('path');
 const express = require("express");
 const app = express();
 
 //FORMER CSV PARSING CODE
-let abbrevs = [];
-fs.createReadStream('abbreviations.csv')
-  .pipe(csv())
-  .on('data', (data) => {
-    abbrevs.push(data)
-    // console.log("abbrevs data[0]:===============================================", data)
-  })
-  .on('end', () => {
-    // console.log("abbrevs:===================================================", abbrevs);
-  });
+// let abbrevs = [];
+// fs.createReadStream('abbreviations.csv')
+//   .pipe(csv())
+//   .on('data', (data) => {
+//     abbrevs.push(data);
+//     // console.log("abbrevs data[0]:===============================================", data)
+//   })
+//   .on('end', () => {
+//     // console.log("abbrevs:===================================================", abbrevs);
+//   });
 
-let state_data = [];
-fs.createReadStream('state_data.csv')
-  .pipe(csv())
-  .on('data', (data) => state_data.push(data))
-  .on('end', () => {
-    // console.log("state_data:===================================================", state_data[0]["state"]);
-  });
+// let state_data = [];
+// fs.createReadStream('state_data.csv')
+//   .pipe(csv())
+//   .on('data', (data) => state_data.push(data))
+//   .on('end', () => {
+//     // console.log("state_data:===================================================", state_data[0]["state"]);
+//   });
 
 
 //RENDER HTML
@@ -32,41 +33,44 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-  //This will return all the state/abbreviations
+//This will return all the state/abbreviations
 let all_data = [];
 app.get("/states", function(req, res) {
   //HERE WE NEED TO ADD STATE NAME AND ABBREVS INTO ONE ARRAY
   //FOR EACH ITEM IN ARRAY, CREATE A NEW HASH --> hash[state_data["state"]]= abbrevs["Code"]
-    //THEN ADD THIS OBJECT/HASH TO ARRAY
+  //THEN ADD THIS OBJECT/HASH TO ARRAY
+  const state_data = get_states();
+  const abbrevs = get_abbrevs();
   let index = 0;
   let state = "";
-  let abbrev = ""
-  console.log(`state data: ${state_data}`)
-  console.log(`abbrevs: ${abbrevs}`)
-  while (index < state_data.length){
+  let abbrev = "";
+  console.log(`state data: ${state_data}`);
+  // console.log(`abbrevs: ${abbrevs}`);
+  while (index < state_data.length) {
     state = state_data[index]["state"];
     abbrev = abbrevs[index]["Code"];
-    all_data.push(`${state}, ${abbrev}`)
+    all_data.push(`${state}, ${abbrev}`);
     index += 1;
   }
   res.send(all_data);    
 });
   
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("server is running on port 3000");
-})
+});
 
 //this will parse params and return data for the specified state
-app.get('/states/:abbreviation', function(req, res){
+app.get('/states/:abbreviation', function(req, res) {
   //we need to associate an ABREVIATION with the correct STATE DATA
   let state = "";
   // let temp_abbrev = "";
+  const abbrevs = get_abbrevs;
   let index = 0;
-  while(index < abbrevs.length){
-    if (abbrevs[index]["Code"] == req.params.abbreviation){
+  while (index < abbrevs.length) {
+    if (abbrevs[index]["Code"] == req.params.abbreviation) {
       console.log(abbrevs[index]["State"]);
       // temp_abbrev = abbrevs[index]["State"];
-      state = `${abbrevs[index]["State"]}, ${req.params.abbreviation} `
+      state = `${abbrevs[index]["State"]}, ${req.params.abbreviation} `;
       break;
     } 
     index += 1;    
